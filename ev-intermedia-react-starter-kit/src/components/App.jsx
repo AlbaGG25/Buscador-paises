ed8272136edece139d77986803fe198fa14ebccc
“Alba” <albaginesgarcia25@gmail.com>
2023-12-22 19:10:02 +0100

    title changed

--- ev-intermedia-react-starter-kit/src/components/App.jsx
+++ ev-intermedia-react-starter-kit/src/components/App.jsx
@@ -55,8 +55,7 @@ const App = () => {
       <header className="header">
         <h1>Country info App</h1>
         <p>
-          Explore information about countries, capitals and flags. Add new
-          countries and filter through the list!
+          Explore information about countries, capitals and flags. Filter through the list!
         </p>
       </header>
       <main className="main">
